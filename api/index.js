7910c258aad227fa136b91bc6ea62cd4f3a90446
jagwinder singh <78211341+jagwinder8006@users.noreply.github.com>
2025-04-20 22:10:45 +0530

    fix deploy issues

--- api/index.js
+++ api/index.js
@@ -3,13 +3,11 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
   res.send('Hello World! from Vercel');
 });
 
-app.get('/twitter', (req, res) => {
-  res.send('<h1>Hello ji, doston ki haal chaal</h1>');
-});
+
 
 // Export the app and a serverless handler
 module.exports = app;
