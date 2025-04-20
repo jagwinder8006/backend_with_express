327eb24ace66400cc286ba6bd9ecea40223f37f5
jagwinder singh <78211341+jagwinder8006@users.noreply.github.com>
2025-04-20 22:49:49 +0530

    fix vercel

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const serverless = require('serverless-http');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from Vercel');
+  res.send('Hello from root index.js on Vercel!');
 });
 
+app.get('/twitter', (req, res) => {
+  res.send('<h1>Hello from /twitter route!</h1>');
+});
 
-
-// Export the app and a serverless handler
 module.exports = app;
 module.exports.handler = serverless(app);
+
